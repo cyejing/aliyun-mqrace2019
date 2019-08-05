620211e0004c4ea4233bdf991ade4f52991bc286
Born <jing23501@qq.com>
2019-08-05 10:43:35 +0800

    init

--- src/main/java/io/openmessaging/GlobalConfig.java
+++ src/main/java/io/openmessaging/GlobalConfig.java
@@ -5,10 +5,10 @@ package io.openmessaging;
  */
 public interface GlobalConfig {
 
-    //    String BombFile = "/alidata1/race2019/data/bomb-";
-//    String IndexFile = "/alidata1/race2019/data/index-";
-    String BombFile = "bomb-";
-    String IndexFile = "index-";
+        String BombFile = "/alidata1/race2019/data/bomb-";
+    String IndexFile = "/alidata1/race2019/data/index-";
+//    String BombFile = "bomb-";
+//    String IndexFile = "index-";
     int QueueSize = 1024 * 20;
     int BombSize = 64 * 1024;
     int IndexSize = (64 * 1024) / 16;
